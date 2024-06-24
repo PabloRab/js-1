3029ca2bdcf06ca34f461935ab0a1965034b9cd5
PabloRab <142822900+PabloRab@users.noreply.github.com>
2024-06-24 18:23:32 +0300

    пврвпр

---
+++ script.js
@@ -0,0 +1,45 @@
+// "use strict"
+
+
+// console.log('kirill'.length);
+// console.log('kirill'.toUpperCase());
+// let age = 25;
+
+// age = 30;
+// console.log(age);
+
+
+// const a = 5;
+// const b = 25.5;
+
+
+// const c = 'hello';
+
+// const d = true;
+
+// const isModalOpen = false;
+
+// const selectedProduct = null;
+
+// let userName;
+// userName = 'Nick';
+// console.log(userName)
+
+// console.log(typeof c);
+// console.log(typeof userName);
+
+
+// const message = 'Hello GoITeens';
+// console.log(massage);
+
+// const userName = 'Nick';
+// console.log("Username is", userName);
+// alert(message);
+
+// const message = confirm("Чи згодні з умовами?");
+// console.log(message);
+
+const message = prompt("Вкажіть ваш вік");
+console.log(message);
+
+
