f09d67710cb870e4adf3d817bfcb511e7e8166ad
PabloRab <142822900+PabloRab@users.noreply.github.com>
2024-07-01 20:42:51 +0300

    /

---
+++ js3/script.js
@@ -0,0 +1,53 @@
+// const message = "Hello" + " " + "JavaScript";
+// console.log(message);
+
+
+// const result = 6 + '6';
+// console.log(result);
+
+
+// const number = 5 + 5 + '5';
+// console.log(number);
+// console.log(typeof number);
+
+
+// const a = "My name is Kirill ";
+// console.log(a.length);
+
+// const b = "welcome to New York";
+// console.log(b.toLowerCase());
+// console.log(b.toUpperCase());
+// console.log(b);
+
+// console.log(b.indexOf('to'));
+// console.log(b.indexOf('GoITeens'));
+
+// console.log(b.includes('New'));
+
+// console.log(b.startsWith('wel'));
+// console.log(b.endsWith('.'))
+
+// console.log(b.length);
+// console.log(b.padEnd(25, '!'))
+// console.log(b.padStart(21, '@'))
+
+
+
+// const name = 'Nick';
+// const age = 15;
+// const mood = 'happy';
+
+// const mes = 'My name is ' + name + ', I`m ' + age + ' years old and ' + mood + '.';
+// console.log(mes);
+
+// const sameMes = `My name is ${name}, I'm ${age} years old and ${mood}.`;
+// console.log(sameMes);
+
+// console.log(`Сума покупки: ${121 + 58}`);
+
+
+const a = 'hi qwerty';
+console.log(a.startsWith('h'));
+console.log(a.endsWith('!'));
+
+
