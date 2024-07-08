35f043974400c0fab65941bb05235d40d4765dbf
PabloRab <142822900+PabloRab@users.noreply.github.com>
2024-07-09 00:22:50 +0300

    .

---
+++ js4/script.js
@@ -0,0 +1,65 @@
+// const num = 25;
+// const result = num > 15 && num < 35;
+// console.log(result);
+
+// const number = 40;
+// const res = number > 15 && number < 35;
+// console.log(res);
+
+
+
+// const num = 40;
+// const result = num < 10 || num > 30;
+// console.log(result);
+
+// const num = 20;
+// const result = num < 10 || num > 30;
+// console.log(result);
+
+
+// let cost = 0;
+// const sub = 'pro';
+
+// if (sub === 'premium') {
+//     cost = 100;
+// }
+// console.log(cost);
+
+// let cost;
+// const sub = 'free';
+// if (sub === 'free') {
+//     cost = 100;
+// } else {
+//     cost = 0;
+// }
+
+// console.log(cost);
+
+
+// let cost;
+// const sub = 'premium';
+
+// if (sub === 'free') {
+//     cost = 0;
+// } else if (sub === 'pro') {
+//     cost = 100;
+// } else if (sub === 'premium') {
+//     cost = 250;
+// } else {
+//     console.log('Ви не підписані');
+// }
+
+// let type;
+// const age = 16;
+
+// if (age >= 18) {
+//     type = 'adult';
+// } else {
+//     type = 'child';
+// }
+// console.log(type);
+
+const num1 = 5;
+const num2 = 10;
+const bigger = num1 > num2 ? num1 : num2;
+console.log(bigger);
