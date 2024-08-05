90c0a638340d8dbf78550af7201c689011d6c701
PabloRab <142822900+PabloRab@users.noreply.github.com>
2024-08-05 18:22:54 +0300

    .ю.

---
+++ js5/script.js
@@ -0,0 +1,36 @@
+// let counter = 0;
+
+// while (counter < 10) {
+//     console.log('counter: ', counter);
+//     counter += 1;
+// }
+
+// let clients = 12;
+// const maxClients = 20;
+// while (clients < maxClients) {
+//     console.log(clients);
+//     clients += 1;
+// }
+
+// let password = '';
+// do {
+//     password = prompt('Ведіть пароль довше 4 сисволів', '')
+// } while (password.length < 5);
+// console.log('Ваш пароль:', password);
+
+// const max= 15;
+// for( let i = 0; i < max; i += 2){
+//     console.log(i);
+// }
+
+// const target = 4;
+// let sum = 0;
+// for(let i = 0; i <= target; i += 1){
+//     sum += i;
+// }
+// console.log(sum);
+
+const max = 10;
+for(let i =0; i < max; i += 1) {
+    console.log(`${max} % ${i} =`, max % i);
+}
