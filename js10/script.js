ae63c380dd7b539787cbc3340e199a5511f24d69
PabloRab <142822900+PabloRab@users.noreply.github.com>
2024-08-19 21:18:03 +0300

    .

---
+++ js10/script.js
@@ -0,0 +1,40 @@
+// const filter = function (array, test) {
+//     for (const element of array) {
+//         test(element)
+//     }
+//     return newfilter;
+// }
+
+// const filter = function (array, test) {
+//     const newFilter = [];
+//     for(const element of array) {
+//         const passed = test(element);
+//         if (passed){
+//             newFilter.push(element);
+//         }
+//     }
+//     return newFilter;
+// }
+
+// const fruits = [
+//     {name: 'kiwi', quantity: 300, isFresh: true},
+//     {name: 'orange', quantity: 100, isFresh: false},
+//     {name: 'apples', quantity: 88, isFresh: true},
+// ]
+
+// const freshFruits = filter(fruits, fruit => fruit.isFresh);
+// console.log(freshFruits);
+
+
+
+console.log(value);
+const value = 5;
+console.log(value);
+
+if (true) {
+    console.log(value);
+    const value = 10;
+    console.log(value);
+}
+
+console.log(value);
